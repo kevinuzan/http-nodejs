9cef2c6b8523aa6671eb0c9c7320a4503f245da9
kevinuzan <kevinuzan@hotmail.com>
2023-02-19 19:01:05 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,3 @@
 import { createServer } from 'http';
 
-createServer((req, res) => {
-  res.write('Gabriel troxao');
-  res.end();
-}).listen(process.env.PORT);
+createServer('./html/loginWindow.html');
