21d3bcc7f394e6cc95a9e92e2382cd3526a4e0a6
kevinuzan <kevinuzan@hotmail.com>
2023-02-19 19:12:24 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,1 +1,20 @@
-window.open('./html/loginWindow.html');
+// import { createServer } from 'http';
+
+// createServer((req, res) => {
+//   res.('./html/loginWindow.html');
+//   res.end();
+// }).listen(process.env.PORT);
+
+
+var http = require('http')
+fs = require('fs');
+fs.readFile('./html/loginWindow.html', function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function (request, response) {
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.write(html);
+    response.end();
+  }).listen(8000);
+});
