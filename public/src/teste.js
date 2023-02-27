d25d663ace73622e3a3dc155f22f1500b326b599
kevinuzan <kevinuzan@hotmail.com>
2023-02-27 20:08:03 -0300

    Update teste.js

--- public/src/teste.js
+++ public/src/teste.js
@@ -2,6 +2,14 @@ async function teste() {
   document.getElementById("teste").value = "oi"
 }
 
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 async function abrirpg2() {
   window.location.replace(__dirname + '/html/index2.html')
 }
