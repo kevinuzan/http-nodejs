8d3e62dda67b7f300a30b350f6228c160334bc4d
kevinuzan <kevinuzan@hotmail.com>
2023-02-27 20:37:57 -0300

    module on teste.js

--- public/src/teste.js
+++ public/src/teste.js
@@ -2,9 +2,18 @@ async function teste() {
   document.getElementById("teste").value = "oi"
 }
 
-const path = require("path")
+// const path = require("path")
 
-const __dirname = path.dirname(filename);
+// const __dirname = path.dirname(filename);
+
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 async function abrirpg2() {
   window.location.replace(__dirname + '/html/index2.html')
