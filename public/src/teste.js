960fb1de256555b30dbdaa7459bf1423dbac744f
kevinuzan <kevinuzan@hotmail.com>
2023-02-27 20:43:16 -0300

    npm install dependences

--- public/src/teste.js
+++ public/src/teste.js
@@ -1,3 +1,12 @@
+
+import path from '../../node_modules/path';
+import { fileURLToPath } from '../../node_modules/url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+
 async function teste() {
   document.getElementById("teste").value = "oi"
 }
@@ -8,12 +17,6 @@ async function teste() {
 
 
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 async function abrirpg2() {
   window.location.replace(__dirname + '/html/index2.html')
