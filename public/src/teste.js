25f60608aa43431e0f2be076fc14c9e123a733ff
kevinuzan <kevinuzan@hotmail.com>
2023-02-27 21:38:58 -0300

    sda

--- public/src/teste.js
+++ public/src/teste.js
@@ -2,8 +2,8 @@
 import path from '/node_modules/path';
 import { fileURLToPath } from '/node_modules/url';
 
-import * as path from 'path'
-import * as fileURLToPath from 'url';
+// import * as path from 'path'
+// import * as fileURLToPath from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
