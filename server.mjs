10964fefc8e35c2404e99c87c8e75b9052636862
kevinuzan <kevinuzan@hotmail.com>
2023-02-27 21:58:12 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -44,9 +44,9 @@ server.listen(process.env.PORT);
 // place all your static files there and the below app.use() will
 // serve all files and sub-directories contained within it.
 app.use(express.static(join(__dirname, 'public')));
-app.use('/src',express.static(path.join(__dirname, 'public/src')));
-app.use('/node_modules',express.static(path.join(__dirname, 'node_modules')));
-app.use('/html',express.static(path.join(__dirname, 'public/html')));
+app.use('/src',express.static(join(__dirname, 'public/src')));
+app.use(express.static(join(__dirname, 'node_modules')));
+app.use('/html',express.static(join(__dirname, 'public/html')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
