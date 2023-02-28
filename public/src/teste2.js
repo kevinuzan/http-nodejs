9cd2d33914132c7ce64fb69db13b552dafea83e9
kevinuzan <kevinuzan@hotmail.com>
2023-02-28 20:43:55 -0300

    Update teste2.js

--- public/src/teste2.js
+++ public/src/teste2.js
@@ -1,9 +1,8 @@
-import { getPage } from "../../server.mjs"
+
 
 async function teste() {
   document.getElementById("teste").value = "oi"
 }
 
 async function abrirpg2() {
-  getPage('/public/html/index_2.html')
 }
