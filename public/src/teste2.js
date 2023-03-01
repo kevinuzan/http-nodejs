080452b1ff3ebcea871c3f6a3e4e8e1a6d0e5425
kevinuzan <kevinuzan@hotmail.com>
2023-03-01 20:37:20 -0300

    Update teste2.js

--- public/src/teste2.js
+++ public/src/teste2.js
@@ -6,7 +6,7 @@ async function teste() {
 
 async function buscaCliente() {
   var nome = document.getElementById("inputCliente").value
-  const resp = await fetch('https://danig-budget.up.railway.app/clienteData?name=' + 'Sasaki', {
+  const resp = await fetch('https://danig-budget.up.railway.app/clienteData?name=' + nome, {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
@@ -16,12 +16,36 @@ async function buscaCliente() {
   // Handle any errors please
 
   const data = await resp.json();
-
+  document.getElementById("inputTelefone").value = data[0]["telefone"]
+  document.getElementById("inputTelhado").value = data[0]["tipo_telhado"]
+  document.getElementById("inputEstado").value = data[0]["estado"]
+  document.getElementById("inputCidade").value = data[0]["cidade"]
+  document.getElementById("inputRua").value = data[0]["rua"]
+  document.getElementById("inputNumero").value = data[0]["numero"]
+  document.getElementById("inputBairro").value = data[0]["bairro"]
+  document.getElementById("inputCep").value = data[0]["cep"]
+  document.getElementById("inputVendedor").value = data[0]["vendedor"]
+  document.getElementById("inputDistribuidora").value = data[0]["distribuidora"]
+  document.getElementById("inputIcms").value = data[0]["icms"]
+  document.getElementById("inputPis").value = data[0]["pis"]
+  document.getElementById("inputCofins").value = data[0]["cofins"]
+  document.getElementById("inputPorcentagem").value = data[0]["porcentagem"]
+  document.getElementById("inputArea").value = data[0]["area"]
+  document.getElementById("inputConsumo").value = data[0]["consumo"]
+  document.getElementById("inputTaxa").value = data[0]["taxa"]
+  document.getElementById("inputIlumPub").value = data[0]["ilum_pub"]
+  document.getElementById("inputUniCons").value = data[0]["unid_consid"]
+  document.getElementById("inputBandeira").value = data[0]["bandeira"]
   console.log(data);
 
   // var data = await httpPost("/clienteData", query)
   // console.log(data)
 }
+
+
+window.onload = async function (event) {
+  await onLoad()
+};
 
 async function onLoad() {
   document.getElementById("inputCliente") = "Sasaki"
