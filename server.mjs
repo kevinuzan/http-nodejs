b5ec9cfd345e56b1830a5ca7632a944b6d5bc813
kevinuzan <kevinuzan@hotmail.com>
2023-03-01 20:24:05 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -44,7 +44,7 @@ app.get('/cliente', async function (req, res) {
 
 app.get('/clienteData', async function (req, res) {
     let name = req.query.name;
-    // var query = "SELECT * FROM ERP where cliente = '" + name + "'"
-    // const { rows } = await pool.query(query)
-    res.json(name)
+    var query = "SELECT * FROM ERP where cliente = '" + name + "'"
+    const { rows } = await pool.query(query)
+    res.json(rows)
 });
