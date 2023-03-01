93d26ec5f5d3ed810935f4b63eed448b9efd41f2
kevinuzan <kevinuzan@hotmail.com>
2023-03-01 19:24:03 -0300

    Update teste2.js

--- public/src/teste2.js
+++ public/src/teste2.js
@@ -5,16 +5,20 @@ async function teste() {
 }
 
 async function buscaCliente() {
-  var query = { query: "SELECT CLIENTE FROM ERP ORDER BY CLIENTE ASC" }
-  var myJsonString = JSON.stringify(query);
-  var data = await httpGet("/cliente", myJsonString)
+  var query = "SELECT CLIENTE FROM ERP ORDER BY CLIENTE ASC"
+  var data = await httpGet("/cliente", query)
   console.log(data)
+
+  // var query = { query: "SELECT CLIENTE FROM ERP ORDER BY CLIENTE ASC" }
+  // var myJsonString = JSON.stringify(query);
+  // var data = await httpGet("/cliente", myJsonString)
+  // console.log(data)
 }
 
 function httpGet(theUrl, sendData) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", theUrl, false); // false for synchronous request
-  xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  // xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
   xmlHttp.send(sendData);
   return xmlHttp.response;
 }
