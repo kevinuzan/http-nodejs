c4427e2ff84dd2832c89ed4626f822bfff8a9be2
kevinuzan <kevinuzan@hotmail.com>
2023-03-01 17:22:18 -0300

    dsa

--- public/src/teste.js
+++ public/src/teste.js
@@ -1,16 +1,5 @@
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-
-async function teste() {
-  document.getElementById("teste").value = "oi"
-}
-
 async function abrirpg2() {
-  httpPost("/excel")
+  window.open("/html/index_2.html")
 }
 
 function httpGet(theUrl)
